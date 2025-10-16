590c87add2221f355cad185e93ed284d5799f7bd
KlasterK <kadakinvi2@gmail.com>
2025-10-16 14:39:26 +0000

    Add tasktypedelegate.cpp

---
+++ tasktypedelegate.cpp
@@ -0,0 +1,35 @@
+#include "tasktypedelegate.h"
+#include "taskitem.h"
+
+QWidget* TaskTypeDelegate::createEditor(QWidget *parent, const QStyleOptionViewItem &option, const QModelIndex &index) const
+{
+    auto *combobox = new QComboBox(parent);
+    combobox->addItem("Work", TaskItem::Work);
+    combobox->addItem("Rest", TaskItem::Rest);
+    combobox->setEditable(false);
+    return combobox;
+}
+
+void TaskTypeDelegate::setEditorData(QWidget *editor, const QModelIndex &index) const
+{
+    auto *combobox = qobject_cast<QComboBox*>(editor);
+    if(combobox == nullptr)
+        return;
+
+    int taskType = index.data(Qt::EditRole).toInt();
+    int cbIndex = combobox->findData(taskType);
+    if(cbIndex >= 0)
+    {
+        combobox->setCurrentIndex(cbIndex);
+    }
+}
+
+void TaskTypeDelegate::setModelData(QWidget *editor, QAbstractItemModel *model, const QModelIndex &index) const
+{
+    auto *combobox = qobject_cast<QComboBox*>(editor);
+    if(combobox == nullptr)
+        return;
+
+    int taskType = combobox->currentData().toInt();
+    model->setData(index, taskType, Qt::EditRole);
+}
--- tasktypedelegate.h
+++ tasktypedelegate.h
@@ -1,59 +1,28 @@
 #ifndef TASKTYPEDELEGATE_H
 #define TASKTYPEDELEGATE_H
 
-#include "taskitem.h"
-
 #include <QComboBox>
 #include <QStyledItemDelegate>
-
 
 class TaskTypeDelegate : public QStyledItemDelegate
 {
 public:
-    explicit TaskTypeDelegate(QObject *parent = nullptr)
-        : QStyledItemDelegate(parent)
-    {}
+    inline explicit TaskTypeDelegate(QObject *parent = nullptr)
+        : QStyledItemDelegate(parent) {}
 
-    inline QWidget* createEditor(
+    QWidget* createEditor(
         QWidget *parent,
         const QStyleOptionViewItem &option,
         const QModelIndex &index
-    ) const override
-    {
-        auto *combobox = new QComboBox(parent);
-        combobox->addItem("Work", TaskItem::Work);
-        combobox->addItem("Rest", TaskItem::Rest);
-        combobox->setEditable(false);
-        return combobox;
-    }
+    ) const override;
 
-    inline void setEditorData(QWidget *editor, const QModelIndex &index) const override
-    {
-        auto *combobox = qobject_cast<QComboBox*>(editor);
-        if(combobox == nullptr)
-            return;
+    void setEditorData(QWidget *editor, const QModelIndex &index) const override;
 
-        int taskType = index.data(Qt::EditRole).toInt();
-        int cbIndex = combobox->findData(taskType);
-        if(cbIndex >= 0)
-        {
-            combobox->setCurrentIndex(cbIndex);
-        }
-    }
-
-    inline void setModelData(
+    void setModelData(
         QWidget *editor,
         QAbstractItemModel *model,
         const QModelIndex &index
-    ) const override
-    {
-        auto *combobox = qobject_cast<QComboBox*>(editor);
-        if(combobox == nullptr)
-            return;
-
-        int taskType = combobox->currentData().toInt();
-        model->setData(index, taskType, Qt::EditRole);
-    }
+    ) const override;
 };
 
 #endif // TASKTYPEDELEGATE_H
